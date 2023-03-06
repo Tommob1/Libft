5b2f5148922c2409b6e60e9ddf91bb0736fbbe09
Brayden Tomlinson <63316447+Tommob1@users.noreply.github.com>
2023-03-06 17:03:28 +1030

    Main file runs function tests

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/06 14:28:45 by btomlins          #+#    #+#             */
-/*   Updated: 2023/03/06 16:25:46 by btomlins         ###   ########.fr       */
+/*   Updated: 2023/03/06 17:02:41 by btomlins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <unistd.h>
@@ -15,6 +15,7 @@
 #include "ft_isdigit.c"
 #include "ft_isalnum.c"
 #include "ft_isascii.c"
+#include "ft_isprint.c"
 
 int main (void)
 {
@@ -40,5 +41,9 @@ int main (void)
 	printf("\nisascii test 2: '3' = %d", ft_isascii('3'));
 	printf("\nisascii test 3: '$' = %d\n", ft_isascii('$'));
 
+	//ft_isprint() tests:
+	printf("\nisprint test 1: 'A' = %d", ft_isprint('A'));
+	printf("\nisprint test 2: '3' = %d", ft_isprint('3'));
+	printf("\nisprint test 3: '$' = %d\n", ft_isprint('$'));
 	return(0);
 }
