c7712542c2c5b8f019c7023818fee995d879a012
Brayden Tomlinson <63316447+Tommob1@users.noreply.github.com>
2023-03-15 13:20:07 +1030

    Copies up to dstsize - 1 characters from the string src to dst.

---
+++ ft_strlcpy.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/15 12:53:53 by btomlins          #+#    #+#             */
+/*   Updated: 2023/03/15 13:16:15 by btomlins         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
+{
+	size_t	i;
+
+	i = 0;
+	if (dstsize)
+	{
+		while (i < dstsize - 1 && src[i])
+		{
+			dst[i] = src[i];
+			i++;
+		}
+		dst[i] = 0;
+	}
+	i = 0;
+	while (src[i])
+		i++;
+	return (i);
+}
