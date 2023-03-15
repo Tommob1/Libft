a9d3dc1c053a8412d12b4e8c84eaaea5fd714a25
Brayden Tomlinson <63316447+Tommob1@users.noreply.github.com>
2023-03-15 11:46:09 +1030

    Copies len bytes from string src to string dst

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,18 +6,33 @@
 /*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/09 14:34:29 by btomlins          #+#    #+#             */
-/*   Updated: 2023/03/14 15:15:09 by btomlins         ###   ########.fr       */
+/*   Updated: 2023/03/15 11:42:55 by btomlins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*memmove(void *dst, const void *src, size_t len)
+void	*ft_memmove(void *dst, const void *src, size_t len)
 {
 	size_t	i;
 
 	if (!dst && !src)
 		return (0);
-	i = 0;
-
+	if (src <= dst)
+	{
+		while (len--)
+		{
+			((unsigned char *)dst)[len] = ((unsigned char *)src)[len];
+		}
+	}
+	else
+	{
+		i = 0;
+		while (i < len)
+		{
+			((unsigned char *)dst)[i] = ((unsigned char *)src)[i];
+			i++;
+		}
+	}
+	return (dst);
 }
