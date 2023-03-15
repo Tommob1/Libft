8f631315232568e52b8749072d2f9518ec6b57eb
Brayden Tomlinson <63316447+Tommob1@users.noreply.github.com>
2023-03-15 12:50:55 +1030

    Compares byte string s1 against byte string s2.

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,17 +6,22 @@
 /*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/13 15:24:56 by btomlins          #+#    #+#             */
-/*   Updated: 2023/03/14 15:12:56 by btomlins         ###   ########.fr       */
+/*   Updated: 2023/03/15 12:48:18 by btomlins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	memcmp(const void *s1, const void *s2, size_t n)
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
 	size_t	i;
 
 	i = 0;
-
+	while (i < n)
+	{
+		if (((unsigned char *)s1)[i] != ((unsigned char *)s2)[i])
+			return (((unsigned char *)s1)[i] - ((unsigned char *)s2)[i]);
+		i++;
+	}
 	return (0);
 }
