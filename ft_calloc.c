a15bf2507573bf8b0f86b54a242f74cef5823130
Brayden Tomlinson <63316447+Tommob1@users.noreply.github.com>
2023-03-16 16:49:09 +1030

    Contiguously allocates enough space for count objects that are size bytes of memory each and returns a pointer to the allocated memory.

---
+++ ft_calloc.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/16 15:54:40 by btomlins          #+#    #+#             */
+/*   Updated: 2023/03/16 16:45:56 by btomlins         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	void	*ptr;
+
+	ptr = malloc(count * size);
+	if (ptr == 0)
+		return (ptr);
+	ft_bzero(ptr, size * count);
+	return (ptr);
+}
