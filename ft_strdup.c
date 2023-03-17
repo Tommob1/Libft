0aaa0442bdcf7814f8583d332292ea81098577a2
Brayden Tomlinson <63316447+Tommob1@users.noreply.github.com>
2023-03-17 10:50:15 +1030

    Allocated sufficient memory for a copy of the string s1, does the copy, and returns a pointer to it.

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/16 16:51:01 by btomlins          #+#    #+#             */
-/*   Updated: 2023/03/16 16:58:31 by btomlins         ###   ########.fr       */
+/*   Updated: 2023/03/17 10:44:17 by btomlins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,22 @@
 
 char	*ft_strdup(const char *s1)
 {
+	char	*dest;
+	int		s1_len;
+	int		i;
 
+	s1_len = 0;
+	while (s1[s1_len])
+		s1_len++;
+	dest = (char *)malloc(sizeof(char) * (s1_len + 1));
+	if (!dest)
+		return (NULL);
+	i = 0;
+	while (i < s1_len)
+	{
+		dest[i] = s1[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
 }
