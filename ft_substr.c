8844d42db8738398e4b56fcaf5c84937e4cb91bd
Brayden Tomlinson <63316447+Tommob1@users.noreply.github.com>
2023-03-20 13:14:21 +1030

    Ft_substr.c functional but norminette error (too many lines)

---
+++ ft_substr.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/17 10:54:48 by btomlins          #+#    #+#             */
+/*   Updated: 2023/03/20 13:13:01 by btomlins         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	size_t	i;
+	size_t	j;
+	char	*str;
+	size_t	s_len;
+
+	s_len = ft_strlen(s);
+	if (!s)
+		return (NULL);
+	if (start >= s_len)
+	{
+		len = 0;
+	}
+	else
+	{
+		if (s_len - start < len)
+		{
+			len = s_len - start;
+		}
+	}
+	str = (char *)malloc(sizeof(char) * (len + 1));
+	if (!str)
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (s[i])
+	{
+		if (len > 0 && i >= start && j < len)
+		{
+			str[j] = s[i];
+			j++;
+		}
+		i++;
+	}
+	str[j] = 0;
+	return (str);
+}
