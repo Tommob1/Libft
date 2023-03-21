3c352590b702463ed6ced65da110ffc48a870519
Brayden Tomlinson <63316447+Tommob1@users.noreply.github.com>
2023-03-21 15:36:31 +1030

    Fixed libft.h norminette errors

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/09 14:04:06 by btomlins          #+#    #+#             */
-/*   Updated: 2023/03/21 15:00:29 by btomlins         ###   ########.fr       */
+/*   Updated: 2023/03/21 15:35:39 by btomlins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,6 +43,6 @@ char	*ft_strdup(const char *s1);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
-char    **ft_split(char const *s, char c);
+char	**ft_split(char const *s, char c);
 
 #endif
