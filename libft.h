5ff1dae3becb43fc76175a869889d91c20d12d2f
Brayden Tomlinson <63316447+Tommob1@users.noreply.github.com>
2023-03-24 14:48:40 +1030

    Added Norminette corrections to libft.h file

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/09 14:04:06 by btomlins          #+#    #+#             */
-/*   Updated: 2023/03/24 14:35:18 by btomlins         ###   ########.fr       */
+/*   Updated: 2023/03/24 14:47:27 by btomlins         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,6 +59,6 @@ void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
 t_list	*ft_lstnew(void *content);
-void    ft_lstadd_front(t_list **lst, t_list *new);
+void	ft_lstadd_front(t_list **lst, t_list *new);
 
 #endif
