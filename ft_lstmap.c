9748a376690c331bd3bb9e4daa5e556efdbb6f60
Brayden Tomlinson <63316447+Tommob1@users.noreply.github.com>
2023-03-27 12:41:30 +1030

    Iterates the list ’lst’ and applies the function ’f’ on the content of each node. Creates a new list resulting of the successive applications of the function ’f’

---
+++ ft_lstmap.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: btomlins <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/27 12:23:56 by btomlins          #+#    #+#             */
+/*   Updated: 2023/03/27 12:37:46 by btomlins         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstmap(t_list *lst, void*(*f)(void *), void (*del)(void *))
+{
+	t_list	*new;
+	t_list	*save;
+
+	if (!lst || !f || !del)
+		return (0);
+	new = ft_lstnew(f(lst->content));
+	if (!new)
+		return (0);
+	save = new;
+	lst = lst->next;
+	while (lst)
+	{
+		new->next = ft_lstnew(f(lst->content));
+		if (!new->next)
+		{
+			ft_lstclear(&save, del);
+			return (0);
+		}
+		new = new->next;
+		lst = lst->next;
+	}
+	new->next = 0;
+	return (save);
+}
